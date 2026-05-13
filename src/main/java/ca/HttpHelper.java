cd259d31d2be2bbc44a46bbc2f21684efe9219f1
Cal Alstad <Cal.Alstad@elev.ga.ntig.se>
2026-05-13 11:42:14 +0200

    fixat med alla metoder i main och kopplat hela http helper, allt som är kvar är att fixa så att felaktig input catchas

--- src/main/java/ca/HttpHelper.java
+++ src/main/java/ca/HttpHelper.java
@@ -2,37 +2,48 @@
 // Filen innehåller hjälpklass för http Get
 package ca;
 
-import java.net.URL;                //Håller webbadressen
-import java.util.ArrayList;
-import com.google.gson.reflect.TypeToken;
+
 import kong.unirest.*;
-import java.lang.reflect.Type;
-import com.google.gson.*;
+
 
 public class HttpHelper {
 
-    //Hämtar text från URL och ger tbx text
+    //Hämtar text från URL (books) och ger tbx text
     public static String getBooks() {
         try {
             //Gör om till URL objekt
             String  url = "http://10.151.168.5:3128/books" ;
             //Öppnar koppling till server
-            HttpResponse<String> response = Unirest(url).asString();
+            HttpResponse<String> response = Unirest.get(url).asString();
 
             String json_data = response.getBody();
             //Sparar text läst från servern
 
-            // ska hämta Böcker
-            ArrayList<Books> books;
+            return json_data; // Skickat tillbaka strängen
+            
 
-            // Skapa en typ som besrkiverArrayLirt<Books>
-            Type typ_arr_books = new TypeToken<ArrayList<Books>>(){}.getType(); 
-
-            Gson gson = new Gson();
-            books = gson.fromJson(json_data, typ_arr_books);
 
         } catch (Exception e) {
-            System.out.println("Fel vid GET: " + e.getMessage());
+            System.out.println("Fel vid GET books: " + e.getMessage());
+            return null; //Om något går fel säg att det inte gick
+        }
+    }
+
+     //Hämtar text från URL (books) och ger tbx text
+    public static String getMagazines() {
+        try {
+            //Gör om till URL objekt
+            String  url = "http://10.151.168.5:3128/magazines" ;
+            //Öppnar koppling till server
+            HttpResponse<String> response = Unirest.get(url).asString();
+
+            String json_data = response.getBody();
+            //Sparar text läst från servern
+
+           return json_data; // Skickat tillbaka strängen
+
+        } catch (Exception e) {
+            System.out.println("Fel vid GET magazines: " + e.getMessage());
             return null; //Om något går fel säg att det inte gick
         }
     }
--- src/main/java/ca/Main.java
+++ src/main/java/ca/Main.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
+import java.net.URL;                //Håller webbadressen
+import kong.unirest.*;
+import java.lang.reflect.Type;
+
 
 
 public class Main {
@@ -32,29 +36,71 @@ public class Main {
                 System.out.println("5. Lägg till bok");
                 System.out.println("6. Lägg till tidning");
                 System.out.println("7. Avsluta");
-                int val = Integer.parseInt( System.out.readln("Ange val: "));
+                String val = System.console().readLine("Ange val: ");
           
 
             //4. Kolla användares val
             switch(val){
                 case "1"-> {
-                    HttpHelper.getBooks();
-                    /*
-                     Skapa en typ som besrkiverArrayLirt<Books>
-                    Type typ_arr_books = new TypeToken<ArrayList<Books>>(){}.getType(); 
-                            Gson gson = new Gson();
-                        ArrayList<Books>    books = gson.fromJson(json_data, typ_arr_books);
-                */
+                    String json = HttpHelper.getBooks();
+
+                    if (json != null) {
+                        Gson gson = new Gson();
+                        Type typ_arr_books = new TypeToken<ArrayList<Books>>(){}.getType();
+                        books = gson.fromJson(json, typ_arr_books);
+                        System.out.println("Böcker har hämtats från servern!");
+                    }
+                    
                 }
-                case "2" -> getMagazines();
-                case "3" -> visaBöcker();
-                case "4" -> VisaMagazin();
-                case "5" -> läggTillBok();
-                case "6" -> läggTillMagazin();
+                case "2" -> {
+                    String json = HttpHelper.getMagazines();
+                    
+                    if (json != null) {
+                    Gson gson = new Gson();
+                    Type typ_arr_magazine = new TypeToken<ArrayList<Magazines>>(){}.getType(); 
+                    magazines = gson.fromJson(json, typ_arr_magazine);
+                    System.out.println("Tidningar har hämtats!");
+                    }
+                }
+                case "3" -> {
+                    System.out.println("\n--- Alla Böcker ---");
+                    for (Books b : books) {
+                    System.out.println(b);
+                    }
+                }
+                case "4" -> {
+                    System.out.println("\n--- Alla Tidningar ---");
+                    for (Magazines m : magazines) {
+                    System.out.println(m);
+                    }
+                }
+                case "5" -> {
+                    //Lägger till ett nytt objekt direkt i books-listan
+                    books.add(new Books(
+                    System.console().readLine("ID: "),
+                    System.console().readLine("Titel: "),
+                    true,
+                    System.console().readLine("Författare: "),
+                    System.console().readLine("Genre (Crime, Drama, Mystery, Adventure, Romance, Fantasy, Thriller och Science Fiction): "),
+                    Integer.parseInt(System.console().readLine("Sidor: "))
+                    ));
+                    System.out.println("Boken sparad lokalt i listan!");
+                }
+                case "6" -> {
+                    // Lägger till ett nytt objekt direkt i magazines-listan
+                    magazines.add(new Magazines(
+                    System.console().readLine("ID: "),
+                    System.console().readLine("Titel: "),
+                    true,
+                    Integer.parseInt(System.console().readLine("Nummer: ")),
+                    System.console().readLine("Kategori: "),
+                    Integer.parseInt(System.console().readLine("År: "))
+                    ));
+                    System.out.println("Tidningen sparad lokalt i listan!");
+                }
                 case "7" -> running = false;
                 default -> System.out.println("Fel: Ange ett nummer mellan 1-7");
             }
           }
-
     }
 }
